51d5834ac8eee0f101f85b74df44b6dab623a717
Nuray <210103331@stu.sdu.edu.kz>
2023-10-23 12:43:00 +0600

    1st image

--- app/src/main/java/com/example/lemonade_app/MainActivity.kt
+++ app/src/main/java/com/example/lemonade_app/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.lemonade_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade_app.ui.theme.Lemonade_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp()
+
                 }
             }
         }
@@ -30,17 +40,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp(){
+    LemonadeWithImage()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lemonade_appTheme {
-        Greeting("Android")
-    }
+fun LemonadeWithImage(modifier:Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+){Column(
+    modifier = Modifier,
+    horizontalAlignment = Alignment.CenterHorizontally
+){
+    Image(
+        painter = painterResource(R.drawable.lemon_tree),
+        contentDescription = null
+
+    )
+    Spacer(modifier = Modifier.height(25.dp))
+    Text(
+        text = stringResource(R.string.lemon_tree)
+
+    )
+}
+
 }
